bb2ddf8e19de545a8ce7bc37ebd6b6451ce30ab9
Pratik2245 <paithankarpratik3@gmail.com>
2025-07-11 23:39:46 +0530

    profile entity and dto added and started with ProfileRespository

---
+++ src/main/java/com/jobportal/Job/Portal/dto/ProfileDTO.java
@@ -0,0 +1,27 @@
+package com.jobportal.Job.Portal.dto;
+
+import com.jobportal.Job.Portal.entity.Profile;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProfileDTO {
+    @Id
+    private Long Id;
+    private String email;
+    private String jobTitle;
+    private String company;
+    private String location;
+    private String about;
+    private List<String> skills;
+    private List<Experience> experiences;
+    private List<Certification> certifications;
+    public Profile toEntity(){
+        return new Profile(this.Id,this.email,this.jobTitle,this.company,this.location,this.about,this.skills,this.experiences,this.certifications);
+    }
+}
